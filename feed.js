da9874b7ffe5f7e8c106e45ab2297c8d8b2edbf7
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-06-15 15:10:26 +0530

    converted to pwa

---
+++ feed.js
@@ -0,0 +1,22 @@
+var installButton = document.querySelector('#installapp');
+function openCreatePostModal() {
+  
+  if(defferedPrompt){
+    defferedPrompt.prompt();
+    defferedPrompt.userChoice.then(function(choiceResult){
+      console.log(choiceResult.outcome);
+      if(choiceResult.outcome=== 'dismissed'){
+            console.log('user cancelled installation');
+      } else{
+          
+        console.log('user added the app');
+      }
+
+    });
+    defferedPrompt=null;
+  }
+}
+
+
+
+installButton.addEventListener('click', openCreatePostModal);
